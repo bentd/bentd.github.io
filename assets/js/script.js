508360e987112f76ba05bc35ba5e3524d9725af8
Dylan Bent <code@dylanbent.com>
2018-04-29 18:49:32 -0400

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,44 @@
-function typedFade(self, element1, element2)
+$(document).ready(() =>
 {
-    return (self) =>
+
+    function typedFade(self, element1, element2)
     {
-        setTimeout(() => { $(element1).fadeOut(); }, 2000);
-        setTimeout(() => { $(element2).fadeIn(); }, 2100);
+        return (self) =>
+        {
+            setTimeout(() => { $(element1).fadeOut(); }, 2000);
+            setTimeout(() => { $(element2).fadeIn(); }, 2100);
+        }
     }
-}
 
-var options =
-{
-    stringsElement: "#typed-strings",
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 500,
-    cursorChar: "&#9611;",
-    onComplete: typedFade(self, "#typed-container", "#terminal")
-}
+    var options =
+    {
+        stringsElement: "#typed-strings",
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 500,
+        cursorChar: "&#9611;",
+        onComplete: typedFade(self, "#typed-container", "#terminal")
+    }
 
-var typed = new Typed(".typed", options);
+    var typed = new Typed(".typed", options);
+
+    function datetime()
+    {
+        var d = new Date();
+        var days = ["Sun ", "Mon" , "Tue ", "Wed ", "Thur ", "Fri ", "Sat "];
+        var months = ["Jan ", "Feb ", "Mar ", "Apr ", "May ", "Jun ", "Jul ", "Aug ", "Sep ", "Oct ", "Nov ", "Dec "];
+        var s = "";
+
+        s += days[d.getDay()];
+        s += months[d.getMonth()];
+        s += d.getDate() + "  ";
+        s += d.getHours() + ":";
+        s += d.getMinutes() + ":";
+        s += d.getSeconds();
+
+        return s
+    }
+
+    $("#datetime").append(datetime());
+
+});
